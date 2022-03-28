2aeec8b6cae9938df57d6a6eb4ca3b6bb10ea986
marinamarshall <marinamarshall2@gmail.com>
2022-03-28 19:31:25 +0000

    Rendering Data Part 1

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
+    """
+    item
+    """
     name = models.CharField(max_length=50, null=False, blank=False)
     status = models.BooleanField(null=False, blank=False, default=False)
 
-    def __string__(self):
+    def __str__(self):
+        """
+        string
+        """
         return self.name
