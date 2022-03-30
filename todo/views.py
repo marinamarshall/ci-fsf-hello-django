f2fc3f3e9547e8fe7eb46f7ed1e8b04922863a2f
marinamarshall <marinamarshall2@gmail.com>
2022-03-30 19:05:38 +0000

    Creating a New Item Part 2

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 # Create your views here.
@@ -8,7 +8,7 @@ def get_todo_list(request):
     """
     items = Item.objects.all()
     context = {
-        'items' : items
+        'items': items
     }
     return render(request, 'todo/todo_list.html', context)
 
@@ -17,5 +17,10 @@ def add_item(request):
     """
     add item
     """
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        status = 'status' in request.POST
+        Item.objects.create(name=name, status=status)
+
+        return redirect('get_todo_list')
     return render(request, 'todo/add_item.html')
-    
