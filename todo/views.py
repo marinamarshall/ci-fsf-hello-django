16992cea072c2fa0a1e0535a2e07fdc10d10c4bd
marinamarshall <marinamarshall2@gmail.com>
2022-03-31 20:39:58 +0000

    add URL not working

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from .forms import ItemForm
 
 # Create your views here.
 def get_todo_list(request):
+    """ get todo list """
     items = Item.objects.all()
     context = {
         'items': items
@@ -21,6 +22,7 @@ def add_item(request):
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
+
         form = ItemForm()
         context = {
             'form': form
