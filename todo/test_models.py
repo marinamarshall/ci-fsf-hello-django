32372a8b2034f8e3543d4474115b3d1ee83d5e7f
marinamarshall <marinamarshall2@gmail.com>
2022-04-02 18:02:04 +0000

    Coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -4,5 +4,11 @@ from .models import Item
 
 class TestModels(TestCase):
     def test_done_defaults_to_false(self):
+        """ test_done_defaults_to_false """
         item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.status)
+
+    def test_item_string_method_returns_name(self):
+        """ test_item_string_method_returns_name """
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
--- todo/test_views.py
+++ todo/test_views.py
@@ -42,3 +42,11 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.status)
+
+    def test_can_edit_item(self):
+        """ test_can_edit_item """
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
